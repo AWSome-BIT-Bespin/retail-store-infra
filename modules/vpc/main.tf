c2fd13946a23e4118e3140874c30b7b23b091588
yheelearncode <yonghi01@naver.com>
2026-08-31 16:34:30 +0900

    added vpc and subnet

---
+++ modules/vpc/main.tf
@@ -0,0 +1,108 @@
+terraform {
+    required_providers {
+      aws = {
+        source = "hasicorp/aws"
+        version = "~> 6.0"
+        }
+    }
+}
+
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "rs_vpc" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "rs-vpc"
+  }
+}
+
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "rs-public-a"
+    Tier = "public"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "rs-public-b"
+    Tier = "public"
+  }
+}
+
+resource "aws_subnet" "private_app_a" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.11.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "rs-private-app-a"
+    Tier    = "private"
+    Purpose = "app"
+  }
+}
+
+resource "aws_subnet" "private_app_b" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.21.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "rs-private-app-b"
+    Tier    = "private"
+    Purpose = "app"
+  }
+}
+
+# -------------------------
+# Private DB Subnets
+# -------------------------
+
+resource "aws_subnet" "private_db_a" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.12.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "rs-private-db-a"
+    Tier    = "private"
+    Purpose = "db"
+  }
+}
+
+resource "aws_subnet" "private_db_b" {
+  vpc_id                  = aws_vpc.rs_vpc.id
+  cidr_block              = "10.0.22.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name    = "rs-private-db-b"
+    Tier    = "private"
+    Purpose = "db"
+  }
+}
