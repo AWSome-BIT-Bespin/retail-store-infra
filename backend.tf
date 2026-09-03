16732c22214f01443c501e18db769ce5e0320a99
yheelearncode <yonghi01@naver.com>
2026-09-03 15:34:16 +0900

    add

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket       = "retail-terraform-state-350606136784-ap-northeast-2-an"
+    key          = "dev/terraform.tfstate"
+    region       = "ap-northeast-2"
+    encrypt      = true
+    use_lockfile = true
+  }
+}
