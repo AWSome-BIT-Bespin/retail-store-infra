a906864d29c28dcd0aa0b1f364ce766a7b176b6f
Minseok <flyingpingna2109@gmail.com>
2026-09-03 15:46:09 +0900

    add auto cluster.sh

--- yaml/create-cluster.sh
+++ yaml/create-cluster.sh
@@ -48,3 +48,22 @@ echo "[2/3] cluster.yaml 생성 중..."
 envsubst '${VPC_ID} ${APP_a} ${APP_c}' < cluster.template.yaml > cluster.yaml
 
 echo "[3/3] 완료! cluster.yaml 파일이 생성되었습니다."
+
+# 클러스터 생성 (선택 사항)
+echo "[4/4] EKS 클러스터 생성 시작..."
+eksctl create cluster -f cluster.yaml
+# 생성 완료 후 443 포트 추가
+echo "[5/5] EKS 클러스터 보안 그룹에 443 포트 오픈 중..."
+CLUSTER_SG=$(aws eks describe-cluster \
+  --name "retailstore-eks-cluster" \
+  --region "$REGION" \
+  --query "cluster.resourcesVpcConfig.clusterSecurityGroupId" \
+  --output text)
+# 이미 룰이 존재할 때 에러가 발생하지 않도록 예외 처리 권장
+aws ec2 authorize-security-group-ingress \
+  --group-id "$CLUSTER_SG" \
+  --protocol tcp \
+  --port 443 \
+  --cidr "0.0.0.0/0" \
+  --region "$REGION" || echo "⚠️ 이미 등록된 규칙이거나 실패했습니다."
+echo "✅ 모든 작업이 완료되었습니다."
