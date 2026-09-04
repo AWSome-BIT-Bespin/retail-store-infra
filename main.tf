537618b4466e573384985edbe5b9823aa94a5d2e
yheelearncode <yonghi01@naver.com>
2026-09-04 10:39:10 +0900

    add

---
+++ main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-northeast-2"
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
